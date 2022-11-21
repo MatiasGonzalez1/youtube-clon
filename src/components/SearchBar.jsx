f11b2e636e1942028b16d9a3f84d8ac612d35e98
Matias Gonzalez <mng_epson@hotmail.com>
2022-11-21 20:58:09 -0300

    feat: add searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 import {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import {Paper, IconButton} from '@mui/material';
-import {SearchIcon} from '@mui/icons-material';
+import {Search} from '@mui/icons-material';
 
 const SearchBar = () => {
   return (
@@ -11,8 +11,19 @@ const SearchBar = () => {
     }}
     sx={{
       borderRadius: 20,
+      border: '1px solid #e3e3e3',
+      pl:2,
+      boxShadow:'none',
+      mr:{sm:5}
     }}
-    >SearchBar</Paper>
+    >
+      <input
+      className='search-bar' placeholder='search...' value='' onChange={()=>{}}
+      />
+      <IconButton type='submit' sx={{p: '10px', color: 'red'}}>
+        <Search/>
+      </IconButton>
+    </Paper>
   )
 }
 

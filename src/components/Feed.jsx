20d178f39fb3fdaafa080859b1d31461380912f6
Matias Gonzalez <mng_epson@hotmail.com>
2022-11-22 00:13:08 -0300

    feat: add fetch to rapidAPI

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,15 +1,31 @@
 import { useState, useEffect } from "react";
-import {Box, Stack, Typography} from '@mui/material'
-import Sidebar from "./Sidebar";
+import {Box, Stack, Typography} from '@mui/material';
+import {Sidebar, Videos} from "./";
+import {fetchFromAPI} from '../utils/fetchFromAPI';
 
 const Feed = () => {
+
+  const [selectedCategory, setselectedCategory] = useState('New')
+
+  useEffect(()=>{
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{flexDirection: {sx: 'column', md: 'row'}}}>
       <Box sx={{height: {sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: {sx: 0, md: 2}}}>
-    <Sidebar></Sidebar>
+    <Sidebar selectedCategory = {selectedCategory} setselectedCategory = {setselectedCategory}/>
     <Typography className='copyright' variant='body2' sx={{mt:1.5, color: '#fff'}}>
       Copyrigt 2022 MgDev
     </Typography>
+      </Box>
+      <Box p={2} sx={{overflowY: 'auto',
+    height: '90vh', flex: 2}}>
+        <Typography variant='h4' fontWeight='bold' mb={2} sx={{color: 'white'}}>
+        {selectedCategory}<span style={{color: '#F31503'}}> videos
+          </span>
+        </Typography>
+        <Videos videos={[]}/>
       </Box>
     </Stack>
   )
